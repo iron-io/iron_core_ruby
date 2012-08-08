e8c3a0af2e3452b2cf39ca6f15a07219ab14372b
Michael Fellinger <m.fellinger@gmail.com>
2012-08-08 22:37:51 +0200

    add require_relative

--- lib/iron_core.rb
+++ lib/iron_core.rb
@@ -1,3 +1,6 @@
+# shim for 1.8
+require File.expand_path('../3rdparty/facets/require_relative', __FILE__)
+
 require_relative 'iron_core/version'
 require_relative 'iron_core/logger'
 require_relative 'iron_core/client'
